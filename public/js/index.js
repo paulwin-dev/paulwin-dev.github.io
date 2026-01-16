b7b233b2a2ed0542ce8a3c74ba2d48111b95b2c4
ObSp <150492054+ObSp@users.noreply.github.com>
2026-01-16 20:51:15 +0100

    FInished games section, replaced broken links.

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,5 @@
 import technologies from "./docs/technologies.json"  with { type: "json" }
+import projects from "./docs/projects.json"  with { type: "json" }
 
 function createTechListItem(tech) {
     const techListItem = document.createElement("div");
@@ -24,10 +25,74 @@ function createTechListItem(tech) {
     return techListItem;
 }
 
-//load technology items dybnamically from JSON
-const techListContainer = document.getElementById("tech-list-container")
+function loadGames() {
+    const gamesContainer = document.getElementById("work-list-container");
+    const template = document.getElementById("work-list-item-template");
+
+    for (const project of projects) {
+        const projectItem = template.content.cloneNode(true);
+        const projectCover = projectItem.querySelector(".work-list-item-cover");
+        const projectHeader = projectItem.querySelector(".work-list-item-header");
+        const projectDesc = projectItem.querySelector(".work-list-item-desc");
+        const projectTags = projectItem.querySelector(".work-list-item-tags-container");
+
+        projectHeader.textContent = project.name;
+        projectDesc.textContent = project.description;
+        projectCover.src = project.cover;
+        
+        for (const tag of project.tags) {
+            const tagElement = document.createElement("span");
+            tagElement.className = "work-list-item-tag";
+            tagElement.textContent = tag;
+
+            projectTags.appendChild(tagElement);
+        }
+
+        projectItem.querySelector(".work-list-item").onclick = () => {
+            window.open(project.link, "_blank");
+        }
+
+        projectItem.querySelector(".work-list-item").setAttribute("data-section", project.section);
+        
+        gamesContainer.appendChild(projectItem);
+    }
+}
+
+function loadGameFilters() {
+    const filterButtons = document.getElementsByClassName("work-filter-item");
+
+    let selectedFilter = "all";
+
+    for (const button of filterButtons) {
+
+        button.onclick = () => {
+            selectedFilter = button.getAttribute("data-section");
+
+            for (const btn of filterButtons) {
+                btn.classList.remove("work-filter-item-selected");
+            }
+            button.classList.add("work-filter-item-selected");
+
+            for (const item of document.getElementsByClassName("work-list-item")) {
+                const section = item.getAttribute("data-section")
+                console.log(section, selectedFilter)
+                if (selectedFilter === "all" || section === selectedFilter) {
+                    item.style.display = "block";
+                } else {
+                    item.style.display = "none";
+                }
+            }
+        }
+
+    }
+}
+
+const techListContainer = document.getElementById("tech-list-container");
 
 for (const tech of technologies) {
     const techListItem = createTechListItem(tech);
     techListContainer.appendChild(techListItem);
 }
+
+loadGames();
+loadGameFilters();
